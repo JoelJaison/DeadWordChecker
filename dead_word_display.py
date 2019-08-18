cdd12d192a22c20dd9158b8ca4fbd4c580d8a587
JoelJaison <joeljaison13@gmail.com>
2019-08-18 16:48:02 -0700

    Added list of starting dead words

--- dead_word_display.py
+++ dead_word_display.py
@@ -1,6 +1,7 @@
 import re
 import os
 from dead_word_counter import deadwordlist, linkverblist
+from processlist import process_list
 """
 
 Will split the text file into paragraphs, replace dead words with appropriate html tags,
@@ -12,6 +13,12 @@ with an html file containing the number of counts of each word
 textfile = input("Enter File Name")
 with open(textfile+".txt") as langessay:
     data = langessay.read()
+startwordlist = None
+with open("StartWords.txt") as starts:
+    for line in starts:
+        startwordlist = line.split(" ")
+startwordlist = process_list(startwordlist)
+print(startwordlist)
 """
 deadwordlist = None
 linkverblist = None
@@ -36,6 +43,8 @@ for index in range(len(parlist)):
         parlist[index] = parlist[index].replace(deadword, "<span style='background-color:yellow'>%s</span>" % deadword)  
     for linkverb in linkverblist:
         parlist[index] = parlist[index].replace(linkverb, "<span style='background-color:yellow'>%s</span>" % linkverb)
+    for startword in startwordlist:
+        parlist[index] = parlist[index].replace(startword, "<span style = 'background-color:yellow'>%s</span>" % startword)
 with open("WordChecker.html","w") as file:
     for paragraph in parlist:
         file.write("<p>%s</p>\n" % paragraph)
--- processlist.py
+++ processlist.py
@@ -1,6 +1,5 @@
 def process_list(list):
     for i in range(len(list)):
-        list[i] = list[i].lower()
         if "-" in list[i]:
             list[i] = list[i].replace("-", " ")
     return list
