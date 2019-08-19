2ac2c6f29db497d83f4e9a05fd8fe82a589272c4
JoelJaison <joeljaison13@gmail.com>
2019-08-18 17:24:04 -0700

    cleanup

--- dead_word_display.py
+++ dead_word_display.py
@@ -1,6 +1,5 @@
 import re
 import os
-from dead_word_counter import deadwordlist, linkverblist
 from processlist import process_list
 """
 
@@ -13,25 +12,12 @@ with an html file containing the number of counts of each word
 textfile = input("Enter File Name")
 with open(textfile+".txt") as langessay:
     data = langessay.read()
-startwordlist = None
-with open("StartWords.txt") as starts:
-    for line in starts:
-        startwordlist = line.split(" ")
-startwordlist = process_list(startwordlist)
-"""
 deadwordlist = None
-linkverblist = None
-with open("DeadWordList.txt") as dwordlist:
-    for line in dwordlist:
+with open("DeadWordList.txt") as dwords:
+    for line in dwords:
         deadwordlist = line.split(" ")
-
-with open("LinkingVerbs.txt") as lverblist:
-    for line in lverblist:
-        linkverblist = line.split(" ")
-linkverblist = process_list(linkverblist)
-
 deadwordlist = process_list(deadwordlist)
-"""
+
 paragraphs = data.split("\n")
 parlist = list()
 for paragraph in paragraphs:
@@ -40,10 +26,7 @@ for paragraph in paragraphs:
 for index in range(len(parlist)):
     for deadword in deadwordlist:
         parlist[index] = parlist[index].replace(" "+deadword+" ", "<span style='background-color:yellow'>%s</span>" % (" "+deadword+" "))  
-    for linkverb in linkverblist:
-        parlist[index] = parlist[index].replace(" "+linkverb+" ", "<span style='background-color:yellow'>%s</span>" % (" "+linkverb+" "))
-    for startword in startwordlist:
-        parlist[index] = parlist[index].replace(" "+startword+" ", "<span style = 'background-color:yellow'>%s</span>" % (" "+startword+" "))
+
 with open("WordChecker.html","w") as file:
     for paragraph in parlist:
         file.write("<p>%s</p>\n" % paragraph)
